166897c19474504d9de2856be261b0f85966f0a1
Chihyun Song <ian0371@gmail.com>
2024-01-21 20:58:05 +0900

    Add tart

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,10 +1,9 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, ...
 }: {
   services.nix-daemon.enable = true;
   programs.zsh.enable = true;
@@ -22,18 +21,18 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   environment.etc =
     lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
 
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -54,6 +53,9 @@
     onActivation.upgrade = true;
     onActivation.autoUpdate = true;
     onActivation.cleanup = "uninstall";
+    brews = [
+      "cirruslabs/cli/tart"
+    ];
     casks = [
       "alfred"
       "bettertouchtool"
