e44e498e51979c632286ed4cdc45d6900663cb1c
Chihyun Song <ian0371@gmail.com>
2025-10-15 16:05:06 +0900

    Add GitLink shortcut

--- home-manager/static/astronvim_v5/lua/plugins/user.lua
+++ home-manager/static/astronvim_v5/lua/plugins/user.lua
@@ -96,7 +96,12 @@ return {
   },
   {
     "linrongbin16/gitlinker.nvim",
-    lazy = false,
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
   },
   {
     "kylechui/nvim-surround",
