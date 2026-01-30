f6efc73d91f4f2f3f49fcfeb4242a143ffc565c0
Chihyun Song <ian0371@gmail.com>
2026-01-31 00:32:52 +0900

    Format with alejandro

--- flake.nix
+++ flake.nix
@@ -8,104 +8,99 @@
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    # rosetta-builder = {
-    #   url = "github:cpick/nix-rosetta-builder";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , darwin
-    , home-manager
-    , ...
-    } @ inputs:
-    let
-      inherit (self) outputs;
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
+  outputs = {
+    self,
+    nixpkgs,
+    darwin,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+
+    darwinConfigWrapper = {
+      system,
+      extraModules ? [],
+    }: (inputs.darwin.lib.darwinSystem {
+      inherit system;
+      specialArgs = {inherit inputs outputs;};
+      modules = [
+        {
+          imports =
+            extraModules
+            ++ [
+              ./nix-darwin/configuration.nix
+            ];
+        }
       ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+    });
+    hmConfigWrapper = {
+      system,
+      extraModules ? [],
+    }: (inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      modules = [
+        {
+          _module.args.self = self;
+          _module.args.inputs = self.inputs;
+          imports =
+            extraModules
+            ++ [
+              ./home-manager/home.nix
+            ];
+        }
+      ];
+      extraSpecialArgs = {inherit inputs outputs;};
+    });
+  in {
+    packages = forAllSystems (system: import ./pkgs {pkgs = nixpkgs.legacyPackages.${system};});
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandro);
 
-      darwinConfigWrapper = { system, extraModules ? [ ] }: (inputs.darwin.lib.darwinSystem {
-        inherit system;
-        specialArgs = { inherit inputs outputs; };
+    overlays = import ./overlays {inherit inputs;};
+    homeManagerModules = import ./modules/home-manager;
+
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
         modules = [
-          # inputs.rosetta-builder.darwinModules.default {
-          #   # see available options in module.nix's `options.nix-rosetta-builder`
-          #   nix-rosetta-builder.onDemand = true;
-          # }
-          {
-            imports =
-              extraModules
-              ++
-              [
-                ./nix-darwin/configuration.nix
-              ];
-          }
+          ./nixos/configuration.nix
         ];
-      });
-      hmConfigWrapper = { system, extraModules ? [ ] }: (inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        modules = [
-          {
-            _module.args.self = self;
-            _module.args.inputs = self.inputs;
-            imports =
-              extraModules
-              ++ [
-                ./home-manager/home.nix
-              ];
-          }
-        ];
-        extraSpecialArgs = { inherit inputs outputs; };
-      });
-    in
-    {
-      packages = forAllSystems (system: import ./pkgs { pkgs = nixpkgs.legacyPackages.${system}; });
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-
-      overlays = import ./overlays { inherit inputs; };
-      homeManagerModules = import ./modules/home-manager;
-
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./nixos/configuration.nix
-          ];
-        };
-      };
-
-      darwinConfigurations = {
-        # scutil --get LocalHostName
-        "Chihyunui-Macmini" = darwinConfigWrapper { system = "aarch64-darwin"; };
-        "ianxxui-MacBookPro" = darwinConfigWrapper { system = "aarch64-darwin"; };
-      };
-
-      homeConfigurations = {
-        song = hmConfigWrapper { system = "aarch64-darwin"; };
-        yum3 = hmConfigWrapper {
-          system = "aarch64-linux";
-          extraModules = [{ home.username = "yum3"; }];
-        };
-        ubuntu = hmConfigWrapper {
-          system = "x86_64-linux";
-          extraModules = [{ home.username = "ubuntu"; }];
-        };
-        ec2-user = hmConfigWrapper {
-          system = "x86_64-linux";
-          extraModules = [{ home.username = "ec2-user"; }];
-        };
       };
     };
+
+    darwinConfigurations = {
+      # scutil --get LocalHostName
+      "Chihyunui-Macmini" = darwinConfigWrapper {system = "aarch64-darwin";};
+      "ianxxui-MacBookPro" = darwinConfigWrapper {system = "aarch64-darwin";};
+    };
+
+    homeConfigurations = {
+      song = hmConfigWrapper {system = "aarch64-darwin";};
+      yum3 = hmConfigWrapper {
+        system = "aarch64-linux";
+        extraModules = [{home.username = "yum3";}];
+      };
+      ubuntu = hmConfigWrapper {
+        system = "x86_64-linux";
+        extraModules = [{home.username = "ubuntu";}];
+      };
+      ec2-user = hmConfigWrapper {
+        system = "x86_64-linux";
+        extraModules = [{home.username = "ec2-user";}];
+      };
+    };
+  };
 }
